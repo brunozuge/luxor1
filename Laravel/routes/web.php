72bc448f6331d910ff2df8db75afcdef1f33bd5c
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2026-02-15 16:49:44 -0300

    arrumado o pooler

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -5,11 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/up', function () {
-    return response('OK', 200);
-});
-
-Route::get('/health', function () {
-    return response()->json(['status' => 'ok', 'time' => now()]);
-});
